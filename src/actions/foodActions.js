2f923c66b9d52946b09fdc4c4593f8c0ab3d9c9d
chaijewon <vcandjava@nate.com>
2025-05-27 18:23:44 +0900

    1111

---
+++ src/actions/foodActions.js
@@ -0,0 +1,17 @@
+import {FETCH_FOOD_LIST,FETCH_FOOD_FIND,FETCH_FOOD_DETAIL} from './types'
+import axios from "axios";
+
+export const fetchFoodList = (page) => dispatch => {
+    axios.get('http://localhost/food/list_react',{
+        params: {
+            page: page
+        }
+    }).then((res) => {
+        const action={
+            type:FETCH_FOOD_LIST,
+            payload:res.data
+        }
+        dispatch(action)
+    })
+}
+
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,9 @@ import {combineReducers} from "redux";
 
 // combineReducers : reduce여러개를 모아서 한번에 저장
 import mainReducer from "./mainReducer";
+import foodReducer from "./foodReducer";
 // mains.main_data mains.main_detail => 변수 선택
 export default combineReducers({
-    mains: mainReducer
+    mains: mainReducer,
+    foods: foodReducer
 })
