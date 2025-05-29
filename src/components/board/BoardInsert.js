64b1862a4a77ff5af9a1ac810c2e65081ed95a5b
chaijewon <vcandjava@nate.com>
2025-05-29 12:21:01 +0900

    1111

--- src/components/board/BoardInsert.js
+++ src/components/board/BoardInsert.js
@@ -1,27 +1,68 @@
 import {useDispatch, useSelector} from "react-redux";
-import {useEffect, useState,useRef,Fragment} from "react";
+import {useEffect, useState, useRef, Fragment, use} from "react";
 import {boardInsert} from "../../actions/boardActions";
 import {useNavigate} from "react-router-dom";
 // <unput type="" id , class > <unput type="" ref >
 function BoardInsert() {
-    const dispatch = useDispatch();
-    const nav = useNavigate();
-    const nameRef = useRef(null);
+    const dispatch = useDispatch(); // action함수 호출
+    const nav = useNavigate(); // 화면 이동 , redirect
+    const nameRef = useRef(null); // 태그를 제어할때 사용
     const subjectRef = useRef(null);
     const contentRef = useRef(null);
     const pwdRef = useRef(null);
 
-    const [name, setName] = useState("");
+    const [name, setName] = useState(""); // 데이터를 저장하는 능력이 있는 변수
     const [subject, setSubject] = useState("");
     const [content, setContent] = useState("");
     const [pwd, setPwd] = useState("");
 
     const insert=()=>{
+        if(name.trim()==="")
+        {
+           nameRef.current.focus();
+           return;
+        }
+        else if(subject.trim()==="")
+        {
+            subjectRef.current.focus();
+            return;
+        }
+        else if(content.trim()==="")
+        {
+            contentRef.current.focus();
+            return;
+        }
+        else if(pwd.trim()==="")
+        {
+            pwdRef.current.focus();
+            return;
+        }
+
+        const params={
+            name:name,
+            subject:subject,
+            content:content,
+            pwd:pwd
+        }
+        dispatch(boardInsert(params));
 
     }
+    const result=useSelector(state => state.boards.result)
+    // 조건(X) 항상 밖에 존재
+    if(result && result.msg==='yes'){
+        nav('/board/list')
+    }
+
+    // 이벤트 처리 => 익명의 함수
+    // function Aaa(){}
     const cancel=()=>{
         nav('/board/list')
     }
+
+
+
+
+
 
     return (
        <Fragment>
@@ -83,7 +124,7 @@ function BoardInsert() {
                                  </tr>
                                  <tr>
                                      <td colSpan={"2"} className={"text-center"}>
-                                         <button className={"btn btn-primary btn-sm"}>글쓰기</button>
+                                         <button className={"btn btn-primary btn-sm"} onClick={insert}>글쓰기</button>
                                          <button className={"btn btn-primary btn-sm"} onClick={cancel}>취소</button>
                                      </td>
                                  </tr>
